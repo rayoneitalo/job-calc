09caa1b17cfcbb8f12793b6a0b340b7b7ea210dc
ItaloRayonne <rayoneitalo@gmail.com>
2021-07-31 22:50:59 -0300

    refactor(routes.js): Reset of profile default values; Function for calculating the value of the project extracted for services in order to be reused in other project components

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,13 @@ const views = __dirname + "/views/";
 
 const Profile = {
     data: {
-        name: "Italo Rayone",
-        avatar: "https://github.com/ItaloRayonne.png",
-        monthlyBudget: 3000,
-        daysPerWeek: 5,
-        hoursPerDay: 5,
-        vacationPerYear: 4,
-        valueHour: 45,
+        name: "",
+        avatar: "",
+        monthlyBudget: 0,
+        daysPerWeek: 0,
+        hoursPerDay: 0,
+        vacationPerYear: 0,
+        valueHour: 0,
     },
     controllers: {
         index(req, res) {
@@ -26,7 +26,7 @@ const Profile = {
             const weekTotalHours = data.hoursPerDay * data.daysPerWeek;
             const monthlyTotalHours = weekTotalHours * weeksPerMonth;
 
-            const hours = (data.monthlyBudget / monthlyTotalHours);
+            const hours = data.monthlyBudget / monthlyTotalHours;
 
             Profile.data = {
                 ...Profile.data,
@@ -34,7 +34,7 @@ const Profile = {
                 valueHour: hours,
             };
 
-            return res.redirect('/profile')
+            return res.redirect("/profile");
         },
     },
 };
@@ -53,7 +53,7 @@ const Job = {
                     ...job,
                     remaining,
                     status,
-                    budget: Profile.data.valueHour * job.totalHours,
+                    budget: Job.services.calculateBudget(job, Profile.data.valueHour),
                 };
             });
 
@@ -68,11 +68,9 @@ const Job = {
         },
 
         save(req, res) {
-            const name = req.body.name;
-            const dailyHours = req.body["daily-hours"];
-            const totalHours = req.body["total-hours"];
+            const { name, dailyHours, totalHours } = req.body;
             const createdAt = Date.now();
-            const lastId = Job.data[Job.data.length - 1]?.id || 1;
+            const lastId = Job.data[Job.data.length - 1]?.id || 0;
 
             Job.data.push({
                 id: lastId + 1,
@@ -81,6 +79,55 @@ const Job = {
                 dailyHours,
                 totalHours,
             });
+            return res.redirect("/");
+        },
+
+        show(req, res) {
+            const jobId = req.params.id;
+
+            const job = Job.data.find((job) => Number(job.id) === Number(jobId));
+
+            if (!job) {
+                return res.send("Job not found!");
+            }
+
+            job.budget = Job.services.calculateBudget(job, Profile.data.valueHour);
+
+            return res.render(views + "job-edit", { job });
+        },
+
+        update(req, res) {
+            const jobId = req.params.id;
+
+            const job = Job.data.find((job) => Number(job.id) === Number(jobId));
+
+            if (!job) {
+                return res.send("Job not found!");
+            }
+
+            const updatedJob = {
+                ...job,
+                name: req.body.name,
+                totalHours: req.body.totalHours,
+                dailyHours: req.body.dailyHours,
+            };
+
+            Job.data = Job.data.map((job) => {
+                if (Number(job.id) === Number(jobId)) {
+                    job = updatedJob;
+                }
+
+                return job;
+            });
+
+            res.redirect("/");
+        },
+
+        delete(req, res) {
+            const jobId = req.params.id;
+
+            Job.data = Job.data.filter(job => Number(job.id) != Number(jobId));
+
             return res.redirect("/");
         },
     },
@@ -96,14 +143,18 @@ const Job = {
 
             return dayDiff;
         },
+
+        calculateBudget: (job, valueHour) => valueHour * job.totalHours,
     },
 };
 
 routes.get("/", Job.controllers.index);
 routes.get("/job", Job.controllers.create);
-routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
+routes.post("/job", Job.controllers.save);
+routes.get("/job/:id", Job.controllers.show);
+routes.post("/job/:id", Job.controllers.update);
+routes.post("/job/delete/:id", Job.controllers.delete);
 routes.get("/profile", Profile.controllers.index);
 routes.post("/profile", Profile.controllers.update);
-routes.post("/job", Job.controllers.save);
 
 module.exports = routes;
